9e28ce76e4c640a69fd2e469020a0f983660d012
James Wilson <james@jsdw.me>
2021-12-11 23:33:23 +0000

    trim a few lines from day11

--- src/days/day11.ts
+++ src/days/day11.ts
@@ -3,21 +3,17 @@ import { enumerate } from '../utils'
 export let star1: ThrowingSolver = (input) => {
     let grid = parseInput(input)
     let flashes = 0
-
     for (let i = 0; i < 100; i++) {
         let next = step(grid)
         grid = next.grid
         flashes += next.flashes
     }
-
     return flashes
 }
 
 export let star2: ThrowingSolver = (input) => {
     let grid = parseInput(input)
-
     for (let i = 1;; i++) {
-        console.log(grid)
         let next = step(grid)
         grid = next.grid
         if (next.flashes === 100) return i
@@ -79,7 +75,6 @@ function adjacentMaker(grid: readonly number[][]) {
     function* yieldIfInRange(x: number, y: number): Generator<[number,number],void,unknown> {
         if (isInRange(x, y)) yield [x, y]
     }
-
     return function* coords(x: number, y: number): Generator<[number,number],void,unknown> {
         yield *yieldIfInRange(x-1, y-1)
         yield *yieldIfInRange(x, y-1)
